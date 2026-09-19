5c644d107ab5bfa3606305e561d03b4b36340291
ArchinimeDev <Edsonleonardorojascabia@gmail.com>
2026-09-19 12:13:50 -0500

    subir cambios en clase

--- app/src/main/java/es/oaemdl/cavoshcafe2026/controller/ProdcutoController.java
+++ app/src/main/java/es/oaemdl/cavoshcafe2026/controller/ProdcutoController.java
@@ -1,4 +1,26 @@
 package es.oaemdl.cavoshcafe2026.controller;
 
+import java.util.List;
+
+import es.oaemdl.cavoshcafe2026.ui.Producto;
+
 public class ProdcutoController {
+    this.context =context,
+    db =new Db(context);
+}
+
+public List<Producto> getProductos(boolean bNuevos) {
+    if (bNuevos )db.Sentencias("select * from Productos where");
+    else db.Sentencial( "select * from Productos where Frecuentes")
+    Cursor cursor = db.getCursor();
+
+    List<Producto> productos = null;
+    while ( cursor.moveTonext() ){
+        if ( productos == null)
+            productos
+    }
+
+    cursor.close();
+    return
+
 }
